99fb90311192ab008abdf7bfbf0efd64d707aa2d
RomaniucSebastianBt <sebastian.romaniuc@btrl.ro>
2022-05-24 11:49:31 +0300

    angular app

---
+++ src/app/Employee.ts
@@ -0,0 +1,10 @@
+export interface Employee{
+
+    id : number;
+    salary: number;
+    name: string;
+    adress : string;
+    department: string;
+    email: string;
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeService } from './Employee.service';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
